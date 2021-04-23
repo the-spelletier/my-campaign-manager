65fdde733bbd2a0fc0bfb4c80889184e07f6ff42
Simon <pelletier.simon.18@gmail.com>
2021-04-22 22:14:44 -0400

    Jamais 203

--- config/config.js
+++ config/config.js
@@ -22,6 +22,13 @@ module.exports = {
     "password": DB_PASSWORD,
     "database": DATABASE || "database_production",
     "host": DB_HOST,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "protocol": 'postgres',
+    "dialectOptions": {
+        "ssl": {
+            "require": true,
+            "rejectUnauthorized": false
+        }
+    }
   }
 }
